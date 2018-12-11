c943c73306cc578e36facb069c53fc8bd7a55b6e
ag707 <andygolay@gmail.com>
2018-12-10 19:28:01 -0500

    Update asset_type_register.js

--- asset_type_register.js
+++ asset_type_register.js
@@ -7,40 +7,60 @@ request.responseType = 'json';
 request.send();
       
 request.onload = function () {
-const spec = request.response;
-        showAssetRegister(spec);
+      const spec = request.response;
+      
+      showAssetRegister(spec);
 }
 
 fuction showAssetRegister (jsonObj) {
-  const assets = jsonObj.assets;
-  for (let i = 0; i < assets.length; i++) {  
-    const assetType = document.createElement('p');
-    const code = document.createElement('p');
-    const description = document.createElement('p');
-    const category = document.createElement('p');
-    const legalClassification = document.createElement('p');
-    const assetTypeDiv = document.createElement('div');
-    const codeDiv = document.createElement('div');
-    const descriptionDiv = document.createElement('div');
-    const categoryDiv = document.createElement('div');
-    const legalClassificationDiv = document.createElement('div');
+      const assets = jsonObj.assets;
+      
+      const assetTypeHeader = document.createElement('p');
+      const codeHeader = document.createElement('p');
+      const descriptionHeader = document.createElement('p');
+      const categoryHeader = document.createElement('p');
+      const legalClassificationHeader = document.createElement('p');
+      
+      const assetTypeHeaderDiv = document.createElement('div');
+      const codeHeaderDiv = document.createElement('div');
+      const descriptionHeaderDiv = document.createElement('div');
+      const categoryHeaderDiv = document.createElement('div');
+      const legalClassificationHeaderDiv = document.createElement('div');
 
-    assetType.textContent = assets[i].assetType;
-    code.textContent = assets[i].code;
-    description.textContent = assets[i].description;
-    category.textContent = assets[i].category;
-    legalClassification.textContent = assets[i].legalClassification;
+      assetTypeRegister.appendChild(assetTypeHeaderDiv);
+      assetTypeRegister.appendChild(codeHeaderDiv);
+      assetTypeRegister.appendChild(descriptionHeaderDiv);
+      assetTypeRegister.appendChild(categoryHeaderDiv);
+      assetTypeRegister.appendChild(legalClassificationHeaderDiv);
 
-    assetTypeDiv.appendChild(assetType);
-    codeDiv.appendChild(code);
-    descriptionDiv.appendChild(description);
-    categoryDiv.appendChild(category);
-    legalClassificationDiv.appendChild(legalClassification);
+      for (let i = 0; i < assets.length; i++) {  
+          const assetType = document.createElement('p');
+          const code = document.createElement('p');
+          const description = document.createElement('p');
+          const category = document.createElement('p');
+          const legalClassification = document.createElement('p');
+          const assetTypeDiv = document.createElement('div');
+          const codeDiv = document.createElement('div');
+          const descriptionDiv = document.createElement('div');
+          const categoryDiv = document.createElement('div');
+          const legalClassificationDiv = document.createElement('div');
 
-    assetTypeRegister.appendChild(assetTypeDiv);
-    assetTypeRegister.appendChild(codeDiv);
-    assetTypeRegister.appendChild(descriptionDiv);
-    assetTypeRegister.appendChild(categoryDiv);
-    assetTypeRegister.appendChild(legalClassificationDiv);
-  }
+          assetType.textContent = assets[i].assetType;
+          code.textContent = assets[i].code;
+          description.textContent = assets[i].description;
+          category.textContent = assets[i].category;
+          legalClassification.textContent = assets[i].legalClassification;
+
+          assetTypeDiv.appendChild(assetType);
+          codeDiv.appendChild(code);
+          descriptionDiv.appendChild(description);
+          categoryDiv.appendChild(category);
+          legalClassificationDiv.appendChild(legalClassification);
+
+          assetTypeRegister.appendChild(assetTypeDiv);
+          assetTypeRegister.appendChild(codeDiv);
+          assetTypeRegister.appendChild(descriptionDiv);
+          assetTypeRegister.appendChild(categoryDiv);
+          assetTypeRegister.appendChild(legalClassificationDiv);
+      }
 }
